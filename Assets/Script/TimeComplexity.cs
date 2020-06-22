30c75c4e3391effecda489ea4640a4418922763b
ChanIk Kwon <39299919+lesslate@users.noreply.github.com>
2020-06-23 01:32:19 +0900

    Initial commit

---
+++ Assets/Script/TimeComplexity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class TimeComplexity : MonoBehaviour
+{
+
+    // 실행 시간 측정방법
+    void Start()
+    {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+
+        sw.Stop();
+
+        print(sw.ElapsedMilliseconds.ToString() + "ms");
+    }
+
+
+}
